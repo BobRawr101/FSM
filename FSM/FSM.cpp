d168ffcef2339c144c6912f071c633b46136fa25
BobRawr101 <drizzt199600@gmail.com>
2015-01-04 17:22:26 -0600

    asdf

---
+++ FSM/FSM.cpp
@@ -0,0 +1,32 @@
+// FSM.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+int main()
+{
+	char input;
+	float state;
+	
+	state = 1;
+	input = getchar();
+
+	while (input != '$')
+	{
+		if state == 1
+
+	system("pause"); // creates pause to CMD doesn't immediately close after code is executed.
+	return 0; // I've seen this in every code on the web, I don't know what this does, but I think it does something.
+
+
+	 
